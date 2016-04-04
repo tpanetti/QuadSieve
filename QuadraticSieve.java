15084b6e8935631a1ed3e470d022284998e979bb
tpanetti <tpanetti@email.sc.edu>
2016-04-04 16:33:12 -0400

    added stuff

--- QuadraticSieve.java
+++ QuadraticSieve.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class QuadraticSieve
 {
-
+	private final int SMOOTHNESSBOUND = 1000;
+	
 	public static void main(String[] args)
 	{
 	//Grab number N
@@ -16,17 +19,39 @@ public class QuadraticSieve
 		System.out.println("Enter an N: ");
 		n = input.nextInt();
 	}
+	//Find R?
+	int R = Math.sqrt(n);	//as long as the sqrt is an integer
+	//generate a list of "Big Roots"
+	ArrayList<Integer> primes = erathosthenessSieve(R);
+	
 	
 		
 	
 	
 	}
-
-	public static int[] AtkinSieve(int size) 
+	
+	
+	public static ArrayList<Integer> eratosthenesSieve(int size) 
 	{
-			
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		//populate a list with i = 2 to n
+		for(int i = 2; i < size; i++) 
+			primes.append(i);
+		//set p equal to 2?
+		int p = 2;
+		//loop
+		while(Math.Pow(p, 2) < size)
+		{
+			for(int i = 0; i < primes.size(); i++)
+			{
+				if(primes.get(i) % p == 0)
+					primes.remove(i);
+			}
+		p++
+		}
+		return primes;
 	}
 	
-	public static 
+	
 
 }
