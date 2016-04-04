64af6f4307c9adb33ee0dff6fda02bbefad0f1dd
tpanetti <tpanetti@email.sc.edu>
2016-04-04 16:10:00 -0400

    Added shell of java file

---
+++ QuadraticSieve.java
@@ -0,0 +1,32 @@
+
+public class QuadraticSieve
+{
+
+	public static void main(String[] args)
+	{
+	//Grab number N
+	int n;
+	//take from command line
+	if(args.length == 1)
+		n = args[0];
+	//else take from scanner
+	else
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an N: ");
+		n = input.nextInt();
+	}
+	
+		
+	
+	
+	}
+
+	public static int[] AtkinSieve(int size) 
+	{
+			
+	}
+	
+	public static 
+
+}
